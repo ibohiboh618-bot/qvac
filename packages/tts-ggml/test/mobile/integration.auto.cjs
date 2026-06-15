6c8c64812428114993f697bae948c6faf02c5108
Pratik Narola <pratik.narola@tether.io>
2026-06-16 01:46:33 +0530

    DEBUG fast-ci E1: trim to gpu-smoke + force Supertonic per-step CFM on Android GPU (DO NOT MERGE)

--- packages/tts-ggml/test/mobile/integration.auto.cjs
+++ packages/tts-ggml/test/mobile/integration.auto.cjs
@@ -6,50 +6,10 @@ require('./integration-runtime.cjs')
 
 /* global runIntegrationModule */
 
-async function runAddonTest (options = {}) { // eslint-disable-line no-unused-vars
-  return runIntegrationModule('../integration/addon.test.js', options)
-}
-
-async function runChatterboxJaMecabTest (options = {}) { // eslint-disable-line no-unused-vars
-  return runIntegrationModule('../integration/chatterbox-ja-mecab.test.js', options)
-}
-
-async function runChatterboxMtlTest (options = {}) { // eslint-disable-line no-unused-vars
-  return runIntegrationModule('../integration/chatterbox-mtl.test.js', options)
-}
-
 async function runGpuSmokeTest (options = {}) { // eslint-disable-line no-unused-vars
   return runIntegrationModule('../integration/gpu-smoke.test.js', options)
 }
 
-async function runMultipleRunsTest (options = {}) { // eslint-disable-line no-unused-vars
-  return runIntegrationModule('../integration/multiple-runs.test.js', options)
-}
-
-async function runRtfBenchmarkTest (options = {}) { // eslint-disable-line no-unused-vars
-  return runIntegrationModule('../integration/rtf-benchmark.test.js', options)
-}
-
-async function runStreamingBenchmarkTest (options = {}) { // eslint-disable-line no-unused-vars
-  return runIntegrationModule('../integration/streaming-benchmark.test.js', options)
-}
-
-async function runSupertonicMtlTest (options = {}) { // eslint-disable-line no-unused-vars
-  return runIntegrationModule('../integration/supertonic-mtl.test.js', options)
-}
-
-async function runSupertonicTest (options = {}) { // eslint-disable-line no-unused-vars
-  return runIntegrationModule('../integration/supertonic.test.js', options)
-}
-
 module.exports = {
-  runAddonTest,
-  runChatterboxJaMecabTest,
-  runChatterboxMtlTest,
-  runGpuSmokeTest,
-  runMultipleRunsTest,
-  runRtfBenchmarkTest,
-  runStreamingBenchmarkTest,
-  runSupertonicMtlTest,
-  runSupertonicTest
+  runGpuSmokeTest
 }
