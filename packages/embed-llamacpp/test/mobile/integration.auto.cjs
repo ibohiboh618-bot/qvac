bb482cce70f124544d51c18271531a817f39cf9f
Ridwan Taiwo <donriddo@gmail.com>
2026-06-28 23:43:29 +0100

    exp[skiplog]: regen shards/groups for no-F16 mobile matrix (iOS OOM isolation)

--- packages/embed-llamacpp/test/mobile/integration.auto.cjs
+++ packages/embed-llamacpp/test/mobile/integration.auto.cjs
@@ -95,46 +95,6 @@ async function runBenchmarkPerfEmbeddinggemmaQ80Bs512FaonTest (options = {}) { /
   return runIntegrationModule('../integration/benchmark-perf-embeddinggemma-q8-0-bs512-faon.test.js', options)
 }
 
-async function runBenchmarkPerfQwen3Embedding06bF16Bs1024FaoffTest (options = {}) { // eslint-disable-line no-unused-vars
-  if (typeof __shouldRunTest === 'function' && !__shouldRunTest('runBenchmarkPerfQwen3Embedding06bF16Bs1024FaoffTest')) return __FILTERED
-  return runIntegrationModule('../integration/benchmark-perf-qwen3-embedding-06b-f16-bs1024-faoff.test.js', options)
-}
-
-async function runBenchmarkPerfQwen3Embedding06bF16Bs1024FaonTest (options = {}) { // eslint-disable-line no-unused-vars
-  if (typeof __shouldRunTest === 'function' && !__shouldRunTest('runBenchmarkPerfQwen3Embedding06bF16Bs1024FaonTest')) return __FILTERED
-  return runIntegrationModule('../integration/benchmark-perf-qwen3-embedding-06b-f16-bs1024-faon.test.js', options)
-}
-
-async function runBenchmarkPerfQwen3Embedding06bF16Bs2048FaoffTest (options = {}) { // eslint-disable-line no-unused-vars
-  if (typeof __shouldRunTest === 'function' && !__shouldRunTest('runBenchmarkPerfQwen3Embedding06bF16Bs2048FaoffTest')) return __FILTERED
-  return runIntegrationModule('../integration/benchmark-perf-qwen3-embedding-06b-f16-bs2048-faoff.test.js', options)
-}
-
-async function runBenchmarkPerfQwen3Embedding06bF16Bs2048FaonTest (options = {}) { // eslint-disable-line no-unused-vars
-  if (typeof __shouldRunTest === 'function' && !__shouldRunTest('runBenchmarkPerfQwen3Embedding06bF16Bs2048FaonTest')) return __FILTERED
-  return runIntegrationModule('../integration/benchmark-perf-qwen3-embedding-06b-f16-bs2048-faon.test.js', options)
-}
-
-async function runBenchmarkPerfQwen3Embedding06bF16Bs256FaoffTest (options = {}) { // eslint-disable-line no-unused-vars
-  if (typeof __shouldRunTest === 'function' && !__shouldRunTest('runBenchmarkPerfQwen3Embedding06bF16Bs256FaoffTest')) return __FILTERED
-  return runIntegrationModule('../integration/benchmark-perf-qwen3-embedding-06b-f16-bs256-faoff.test.js', options)
-}
-
-async function runBenchmarkPerfQwen3Embedding06bF16Bs256FaonTest (options = {}) { // eslint-disable-line no-unused-vars
-  if (typeof __shouldRunTest === 'function' && !__shouldRunTest('runBenchmarkPerfQwen3Embedding06bF16Bs256FaonTest')) return __FILTERED
-  return runIntegrationModule('../integration/benchmark-perf-qwen3-embedding-06b-f16-bs256-faon.test.js', options)
-}
-
-async function runBenchmarkPerfQwen3Embedding06bF16Bs512FaoffTest (options = {}) { // eslint-disable-line no-unused-vars
-  if (typeof __shouldRunTest === 'function' && !__shouldRunTest('runBenchmarkPerfQwen3Embedding06bF16Bs512FaoffTest')) return __FILTERED
-  return runIntegrationModule('../integration/benchmark-perf-qwen3-embedding-06b-f16-bs512-faoff.test.js', options)
-}
-
-async function runBenchmarkPerfQwen3Embedding06bF16Bs512FaonTest (options = {}) { // eslint-disable-line no-unused-vars
-  if (typeof __shouldRunTest === 'function' && !__shouldRunTest('runBenchmarkPerfQwen3Embedding06bF16Bs512FaonTest')) return __FILTERED
-  return runIntegrationModule('../integration/benchmark-perf-qwen3-embedding-06b-f16-bs512-faon.test.js', options)
-}
-
 async function runBenchmarkPerfQwen3Embedding06bQ80Bs1024FaoffTest (options = {}) { // eslint-disable-line no-unused-vars
   if (typeof __shouldRunTest === 'function' && !__shouldRunTest('runBenchmarkPerfQwen3Embedding06bQ80Bs1024FaoffTest')) return __FILTERED
   return runIntegrationModule('../integration/benchmark-perf-qwen3-embedding-06b-q8-0-bs1024-faoff.test.js', options)
